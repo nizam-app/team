e5b869a40496a85622ee2e0a9ee9b360743ce7fb
Palash-Chandra-Roy <Palash-Chandra-Roy>
2025-07-16 17:55:39 +0600

    pa

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,6 +15,9 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
+    
+    // hello vai ami polas. 
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
