c65da31147d5441e4861f13132889be021a2936b
nizam-app <appswithnizam@gmail.com>
2025-07-16 15:11:19 +0600

    pitam 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,6 +15,8 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
+    // pitam
+
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
